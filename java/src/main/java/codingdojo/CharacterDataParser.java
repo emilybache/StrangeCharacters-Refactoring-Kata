13a53ccb65b0437045fa9828da03ec54d2c44745
emily <emily@bacheconsulting.com>
2024-07-03 11:09:41 +0200

    r move variable

--- java/src/main/java/codingdojo/CharacterDataParser.java
+++ java/src/main/java/codingdojo/CharacterDataParser.java
@@ -63,7 +63,6 @@ public class CharacterDataParser {
     }
 
     public static Character evaluatePath(String path) {
-        Character character = null;
 
         boolean hasFamilyName = false;
         String characterName = "";
@@ -111,6 +110,7 @@ public class CharacterDataParser {
             tempPathWithoutCurlyBraces.insert(0, "/" + localNameWithoutCurlyBraces);
         }
 
+        Character character = null;
         if (!hasFamilyName)
         {
             character = characterFinder.findByFirstName(characterName);
